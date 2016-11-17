308e50e53cd51d405165301f42330958c3fca57f
Andreas Fischer <andreasfi@me.com>
2016-11-17 09:45:36 +0100

    Add comment

--- ImageEdgeDetectionProject/Program.cs
+++ ImageEdgeDetectionProject/Program.cs
@@ -17,6 +17,7 @@ namespace ImageEdgeDetectionProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // comment
         }
     }
 }
