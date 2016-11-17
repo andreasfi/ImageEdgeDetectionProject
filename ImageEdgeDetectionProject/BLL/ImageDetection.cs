2e734fca3fd9ddfba6afae90a5749d08ba3c2ee5
Andreas Fischer <andreasfi@me.com>
2016-11-17 11:23:59 +0100

    Basic methods

---
+++ ImageEdgeDetectionProject/BLL/ImageDetection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEdgeDetectionProject.BLL
+{
+    class ImageDetection : IImageDetection
+    {
+        IIOfiles fileHandler;
+
+        public ImageDetection()
+        {
+            fileHandler = new FileRW();
+        }
+
+        public Bitmap applyFilter(Bitmap image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bitmap loadImage(string path)
+        {
+            Bitmap image = fileHandler.loadFile(path);
+            return image;
+        }
+
+        public void saveImage(Bitmap image)
+        {
+            fileHandler.saveFile(image);
+        }
+    }
+}
---
+++ ImageEdgeDetectionProject/DAL/IIOfiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEdgeDetectionProject
+{
+    interface IIOfiles
+    {
+        Bitmap loadFile(string path);
+        void saveFile(Bitmap image);
+
+    }
+}
