7dba154dacdc78e4813eff77a5e6ae55a6a57638
Hyrundo <hyrundo.com@gmail.com>
2016-11-22 17:26:46 +0100

    Here is the implemented functionality for image load/filter/show result

--- ImageEdgeDetectionProject/Form1.cs
+++ ImageEdgeDetectionProject/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageEdgeDetectionProject.BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,12 @@ namespace ImageEdgeDetectionProject
 {
     public partial class Form1 : Form
     {
+        ImageDetection anImageDetection;
+
         public Form1()
         {
             InitializeComponent();
+            anImageDetection = new ImageDetection();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -24,17 +28,30 @@ namespace ImageEdgeDetectionProject
 
         private void addImage_Click(object sender, EventArgs e)
         {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            ofd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
 
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image = anImageDetection.loadImage(ofd.FileName);
+            }
         }
 
         private void applyFilter_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = anImageDetection.applyFilter((Bitmap) pictureBox1.Image);
         }
 
         private void saveImage_Click(object sender, EventArgs e)
         {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Specify a file name and file path";
+            sfd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            sfd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
 
+            anImageDetection.saveImage((Bitmap) pictureBox1.Image);
         }
     }
 }
