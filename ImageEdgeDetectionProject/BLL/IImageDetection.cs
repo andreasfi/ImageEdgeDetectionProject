baf2b199a03077f83b45105e7fbbf9a154934034
Andreas Fischer <andreasfi@me.com>
2016-11-24 08:57:55 +0100

    cahnges

--- ImageEdgeDetectionProject/BLL/IImageDetection.cs
+++ ImageEdgeDetectionProject/BLL/IImageDetection.cs
@@ -7,17 +7,11 @@ using System.Threading.Tasks;
 
 namespace ImageEdgeDetectionProject.BLL
 {
-    // this is the creation of the interface relative to 'ImageDetection' Class
-    // this specific interface with its tests list refers exclusively to the BLL-Business Layer Logic main class
+    // this is the interface for the 'ImageDetection' Class
+    // this specific interface for the applyFilter of the programm
+    // IO is also implemented, but calls the DAL class
     public interface IImageDetection
     {
-        //three substitute method calls:
-        // the first one is for the image path to be loaded
-        Bitmap loadImage(string path);
-
-        //the second one is for the image to be saved in the given path
-        void saveImage(Bitmap image, string savepath);
-
         // the third one is for applying the filter to the saved image
         Bitmap applyFilter(Bitmap image);
         
--- ImageEdgeDetectionTest/ImageEdgeDetectionTest.cs
+++ ImageEdgeDetectionTest/ImageEdgeDetectionTest.cs
@@ -12,7 +12,7 @@ namespace ImageEdgeDetectionTest
     [TestClass]
     public class ImageEdgeDetectionTest
     {
-        String path;
+        string path;
 
         //given a string path this test verifies its initialization on an empty value
         Bitmap bitmap;
@@ -26,8 +26,7 @@ namespace ImageEdgeDetectionTest
             nullbitmap = null;
         }
 
-        //this test checks if the outcome of recalling a file through its path is true
-        //through the interface substitute call
+        // Test input and throw execption
         [TestMethod]
         public void correctInputTestWithException()
         {
@@ -39,9 +38,11 @@ namespace ImageEdgeDetectionTest
 
             Bitmap falseimage = im.GetImageFromPath("nopath");
 
+            // If there is a exception the catch will return null
             Assert.AreEqual(falseimage, null);
 
         }
+        // Test input to check if it really is a bitmap
         [TestMethod]
         public void correctInputTestWithoutException()
         {
@@ -53,30 +54,28 @@ namespace ImageEdgeDetectionTest
 
             Bitmap realimage = im.GetImageFromPath("nopath");
 
-            //Assert.IsInstanceOfType(realimage, typeof(Bitmap));
-
+            // Compares if it is bitmaps
             Assert.AreEqual(realimage, bitmap);
 
         }
+        // Test output by throwing an exception
         [TestMethod]
-        
         public void correctOutputTestWithException()
         {
             var testIOFiles = Substitute.For<IIOfiles>();
             var testIImageDetection = Substitute.For<IImageDetection>();
             ImageManagement im = new ImageManagement(testIOFiles, testIImageDetection);
 
-            /*
-            [ExpectedException(typeof(NullReferenceException),
-        "Wrong file path")]
-        */
-
+            // We throw an exception when the methods is used
             testIOFiles.When(x => x.saveFile(bitmap, "nopath"))
                 .Do(x => { throw new NullReferenceException(); });
 
+
             im.SaveImageToPath(bitmap, "nopath");
+            // We get the boolean (that is in ImageManagement) and check if its true
             Assert.IsTrue(im.t);
         }
+        // Test the output 
         [TestMethod]
         public void correctOutputTestWithoutException()
         {
@@ -84,17 +83,18 @@ namespace ImageEdgeDetectionTest
             var testIImageDetection = Substitute.For<IImageDetection>();
             ImageManagement im = new ImageManagement(testIOFiles, testIImageDetection);
             Boolean isok = false;
+
+            // Since its a void method we need to use when do
+            // When the file is written it sets the boolean to true
             testIOFiles.When(x => x.saveFile(bitmap, "nopath"))
                 .Do(x => isok=true );
 
             im.SaveImageToPath(bitmap, "nopath");
 
             Assert.IsTrue(isok);
-
-
         }
 
-        //this test checks if the object path is properly pointing to the suggested root directory for the image when recalled
+        // Test if there is a exception
         [TestMethod]
         public void correctFilterTestWithException()
         {
@@ -102,12 +102,16 @@ namespace ImageEdgeDetectionTest
             var testIImageDetection = Substitute.For<IImageDetection>();
             ImageManagement im = new ImageManagement(testIOFiles, testIImageDetection);
 
+            // It throws an exception
             testIImageDetection.applyFilter(nullbitmap).Returns(k => { throw new Exception(); });
 
             Bitmap falseimage = im.applyTheFilter(nullbitmap);
+
+            // If a exception is trown, applyTheFilter returns null
             Assert.AreEqual(falseimage, null);
         }
 
+        // Test if the filter is applied
         [TestMethod]
         public void correctFilterTestWithOutException()
         {
@@ -115,9 +119,12 @@ namespace ImageEdgeDetectionTest
             var testIImageDetection = Substitute.For<IImageDetection>();
             ImageManagement im = new ImageManagement(testIOFiles, testIImageDetection);
 
+            // The substitute return a bitmap
             testIImageDetection.applyFilter(nullbitmap).Returns<Bitmap>(bitmap);
 
             Bitmap realimage = im.applyTheFilter(bitmap);
+
+            // Check if the initial image is different (filter applied)
             Assert.AreNotEqual(realimage, bitmap);
         }
 
